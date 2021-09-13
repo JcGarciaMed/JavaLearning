ee31710301af3ad84a622c33cab58e188640e676
JORGE <jcgarciamed@gmail.com>
2021-09-12 23:58:59 -0500

    Sorting Arrays

--- ArraysAndLists/src/Arrays.java
+++ ArraysAndLists/src/Arrays.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Arrays {
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         int[] myIntArray = new int[10];
         myIntArray[5]=50;
@@ -15,13 +19,31 @@ public class Arrays {
             myIntArray3[i]= i*10;
             System.out.println("Element: " + i + " , value: " +  myIntArray3[i]);
         }
-
-
-
-
         double[] myDobuleArray = new double[198];
 
+        int[] myIntegers = getIntegers(5);
+        for (int i = 0; i < myIntegers.length; i++) {
+            System.out.println("Element " + i + " , typed value was " + myIntegers[i]);
+        }
 
+        System.out.println("Average is : " + getAverage(myIntegers));
+    }
 
+    public static int[] getIntegers(int numbers){
+        System.out.println("Enter " + numbers + " Integer Values.\r");
+        int[] values = new int[numbers];
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+        return values;
+    }
+
+    public static double getAverage(int[] array){
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return (double) sum/(double) array.length;
     }
 }
