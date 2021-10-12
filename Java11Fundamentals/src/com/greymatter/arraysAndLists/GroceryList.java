007ac843fc06dfbf336f51d4897614fef1075db5
JORGE <jcgarciamed@gmail.com>
2021-10-12 18:39:15 -0500

    List and Arraylist 2

--- Java11Fundamentals/src/com/greymatter/arraysAndLists/GroceryList.java
+++ Java11Fundamentals/src/com/greymatter/arraysAndLists/GroceryList.java
@@ -25,4 +25,12 @@ public class GroceryList {
         String theItem = groceryList.get(position);
         groceryList.remove(position);
     }
+
+    public String findItem(String searchItem){
+        //boolean exists = groceryList.contains(searchItem);
+        if (groceryList.indexOf(searchItem) >= 0){
+            return groceryList.get(groceryList.indexOf(searchItem));
+        }
+        return null;
+    }
 }
