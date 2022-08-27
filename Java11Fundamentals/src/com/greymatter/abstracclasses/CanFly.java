f5afe14ee1651bb4c419313b6f8f0edf270fa5c3
JORGE <jcgarciamed@gmail.com>
2022-08-27 11:02:48 -0500

    Abstract Classes

---
+++ Java11Fundamentals/src/com/greymatter/abstracclasses/CanFly.java
@@ -0,0 +1,8 @@
+package com.greymatter.abstracclasses;
+
+/**
+ * Created by dev on 9/10/2015.
+ */
+public interface CanFly {
+    void fly();
+}
---
+++ Java11Fundamentals/src/com/greymatter/abstracclasses/Dog.java
@@ -0,0 +1,23 @@
+package com.greymatter.abstracclasses;
+
+import com.greymatter.abstracclasses.Animal;
+/**
+ * Created by dev on 7/10/2015.
+ */
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is eating");
+    }
+
+    @Override
+    public void breathe() {
+        System.out.println("Breathe in, breathe out, repeat");
+
+    }
+}
